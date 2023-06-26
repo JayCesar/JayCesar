6d432cd4940af50801ad2448c9c845500423192a
JayCesar <batistajulio215@gmail.com>
2023-06-26 18:36:13 -0300

    @JayCesar - update - add image

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,28 @@
 import React from 'react';
+// Images
+import Image from '../assets/avatar.svg';
+// Icons
+import {FaGithub, FaYoutube, FaDribbble} from 'react-icons/fa';
+// type animation
+import {TypeAnimation} from 'react-type-animation'
+// motion
+import {motion} from 'framer-motion';
+// variants
+import {fadeIn} from '../variants';
 
 const Banner = () => {
-  return <div className='section' id='home'>Banner</div>;
+  return <section className='section' id='home'>
+    <div className="container mx-auto">
+      <div>
+        {/* text */}
+        <div>text</div>
+        {/* image */}
+        <div>
+          <img src={Image} alt='JayCesar_Img' />
+        </div>
+      </div>
+    </div>
+  </section>;
 };
 
 export default Banner;

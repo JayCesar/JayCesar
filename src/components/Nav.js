66bfd75c8bf254fd5fc28ded6469533c5acfca8e
JayCesar <batistajulio215@gmail.com>
2023-06-26 18:30:05 -0300

    @JayCesar - update components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import icons
 import {BiHomeAlt, BiUser} from 'react-icons/bi';
-import {BsClipboardData, BsFillBriefcaseFill, BsChatSquare, BsBriefcase} from 'react-icons/bs';
+import {BsClipboardData, BsFillBriefcaseFill, BsChatSquare, BsBriefcase, BsChatSquareText} from 'react-icons/bs';
 // link
 import {Link} from 'react-scroll';
 
@@ -10,17 +10,45 @@ const Nav = () => {
     <div className="container mx-auto">
       {/*nav inner */}
       <div className='w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50'>
-        <Link className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+        <Link 
+          to='home' 
+          activeClass='active' 
+          smooth={true}
+          spy={true}
+          offset={-200}
+          className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
           <BiHomeAlt />
         </Link>
-        <Link className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+        <Link 
+          to='about'   
+          activeClass='active' 
+          smooth={true}
+          spy={true} 
+          className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
           <BiUser />
         </Link>
-        <Link className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+        <Link 
+          to='services'
+          activeClass='active' 
+          smooth={true}
+          spy={true}
+          className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
           <BsClipboardData />
         </Link>
-        <Link className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+        <Link 
+          to='work' 
+          activeClass='active' 
+          smooth={true}
+          spy={true}
+           className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
           <BsBriefcase />
+        </Link>
+        <Link to='contact' 
+          activeClass='active' 
+          smooth={true}
+          spy={true}
+          className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+          <BsChatSquareText />
         </Link>
       </div>
     </div>

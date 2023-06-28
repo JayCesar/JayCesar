467b4ec16d304265bf2f176b345eb3c2190dacef
JayCesar <batistajulio215@gmail.com>
2023-06-28 20:59:30 -0300

    @JayCesar: update - add responsability to the img

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,9 +11,10 @@ import {motion} from 'framer-motion';
 import {fadeIn} from '../variants';
 
 const Banner = () => {
-  return <section className='section' id='home'>
+  return <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
     <div className="container mx-auto">
-      <div className="flex flex-col gap-y-8 lg:flex-row">
+      <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
+
         {/* text */}
         <div className="flex-1 text-center font-secondary lg:text-left">
           <h1 className="text-[55px] font-bold leading-[0.8] lg:text-[110px]">
@@ -34,8 +35,7 @@ const Banner = () => {
             />
           </div>
           <p className="mb-8 mx-auto lg:max-0">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            <br></br>            
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.      
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
           </p>
           <div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
@@ -59,7 +59,7 @@ const Banner = () => {
         </div>
       
         {/* image */} 
-        <div>
+        <div className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]">
           <img src={Image} alt='JayCesar_Img' />
         </div>
       </div>

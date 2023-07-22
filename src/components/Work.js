aafc34b61f6ea0c134a728ac64a7310ea93f71eb
JayCesar <batistajulio215@gmail.com>
2023-07-22 18:25:37 -0300

    @JayCesar | Update: adds Work section

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,102 @@
-import React from 'react';
+import React from "react";
+// motion
+import { motion } from "framer-motion";
+// variants
+import { fadeIn } from "../variants";
+// img
+import Img1 from "../assets/portfolio-img1.png";
+import Img2 from "../assets/portfolio-img2.png";
+import Img3 from "../assets/portfolio-img3.png";
 
 const Work = () => {
-  return <div className='section' id='work'>Work</div>;
+  return (
+    <section className="section" id="work">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row gap-x-10">
+          <motion.div 
+           variants={fadeIn('right', 0.3)}
+           initial='hidden'
+           whileInView={'show'}
+           viewport={{ once: false, amount: 0.3 }}
+           className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
+            {/*text*/}
+            <div>
+              <h2 className="h2 leading-tigh text-accent">
+                My Latest <br />
+                Work.
+              </h2>
+              <p className='max-w-sm mb-16'>
+                Lorem ipsum dolor sit amet consctetur adipisicing elit.
+                Repellat, hic! Harum quis, aaasdqwdqw velit neqqqwue esse eos molestias
+              </p>
+              <button className='btn btn-sm'>View all projects</button>
+            </div>
+            {/*image*/}
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+              {/*overlay*/}
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 
+              transation-all duration-300'></div>
+              {/*img*/}
+              <img className='group-hover:scale-125 transaction-all duration-500' src={Img1} alt='' />
+              {/* pretittle */}
+              <div className="absolute -bottom-full left-12 
+              group-hover:bottom-24 transation-all duration-500 z-50">
+                <span className='text-gradient'>UI/UX Design</span>  
+              </div>
+              {/* tittle */}
+              <div className="absolute -bottom-full left-12 
+              group-hover:bottom-14 transation-all duration-700 z-50">
+                <span className='text-3xl text-white'>Project Title</span>
+              </div>
+            </div>
+          </motion.div>
+          <motion.div 
+           variants={fadeIn('left', 0.2)}
+           initial='hidden'
+           whileInView={'show'}
+           viewport={{ once: false, amount: 0.3 }}
+           className="flex-1 flex flex-col gap-y-6">
+            {/*image*/}
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+              {/*overlay*/}
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 
+              transation-all duration-300'></div>
+              {/*img*/}
+              <img className='group-hover:scale-125 transaction-all duration-500' src={Img1} alt='' />
+              {/* pretittle */}
+              <div className="absolute -bottom-full left-12 
+              group-hover:bottom-24 transation-all duration-500 z-50">
+                <span className='text-gradient'>UI/UX Design</span>  
+              </div>
+              {/* tittle */}
+              <div className="absolute -bottom-full left-12 
+              group-hover:bottom-14 transation-all duration-700 z-50">
+                <span className='text-3xl text-white'>Project Title</span>
+              </div>
+            </div>
+            {/*image*/}
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+              {/*overlay*/}
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 
+              transation-all duration-300'></div>
+              {/*img*/}
+              <img className='group-hover:scale-125 transaction-all duration-500' src={Img1} alt='' />
+              {/* pretittle */}
+              <div className="absolute -bottom-full left-12 
+              group-hover:bottom-24 transation-all duration-500 z-50">
+                <span className='text-gradient'>UI/UX Design</span>  
+              </div>
+              {/* tittle */}
+              <div className="absolute -bottom-full left-12 
+              group-hover:bottom-14 transation-all duration-700 z-50">
+                <span className='text-3xl text-white'>Project Title</span>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Work;
